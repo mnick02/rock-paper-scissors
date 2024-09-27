7467abf9618f862cd641a780b1434e21c1d4d81b
mnick02 <nokiamn3@gmail.com>
2024-09-27 16:52:52 -0700

    Add conditional statements to return rps string depending on num value

--- rps.js
+++ rps.js
@@ -1,5 +1,14 @@
 //console.log("Hello World");
 function getComputerChoice() {
     num = Math.floor(Math.random() * 60) + 1;
-    //console.log(num);
+    console.log(num);
+    if (num >= 0 && num < 20) {
+        return "rock";
+    }
+    else if (num >= 20 && num < 40) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
 }
