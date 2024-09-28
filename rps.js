8838a36fc979f9a017e3b7fe84db11405e8cb701
mnick02 <nokiamn3@gmail.com>
2024-09-27 17:04:02 -0700

    Add prompt for user to input move and conditional for if input is valid or not

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 //console.log("Hello World");
 function getComputerChoice() {
     num = Math.floor(Math.random() * 60) + 1;
-    console.log(num);
+    //console.log(num);
     if (num >= 0 && num < 20) {
         return "rock";
     }
@@ -10,5 +10,17 @@ function getComputerChoice() {
     }
     else {
         return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose a move:");
+    if (choice.toLowerCase() === "rock" ||
+        choice.toLowerCase() === "paper" ||
+        choice.toLowerCase() === "scissors"){
+            return "valid choice!";
+        }
+    else {
+        return "Sorry, I don't know this move!";
     }
 }
